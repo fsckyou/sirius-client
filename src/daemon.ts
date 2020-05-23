55ccc91ffd907d4c556d1f84ae63282c23836f0a
John Scimone <john@scimone.net>
2020-05-23 19:46:53 -0400

    try fix exiting

--- src/daemon.ts
+++ src/daemon.ts
@@ -118,7 +118,7 @@ const daemon = new Daemon();
 process.on('SIGINT', async () => {
   logger.info('shutting down...');
   await daemon.shutdown();
-  process.kill();
+  process.kill(process.pid, 0);
 });
 
 export default daemon;
