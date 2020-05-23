8546c0349000702d0e2755f4b145a81b3bd12d6c
John Scimone <john@scimone.net>
2020-05-23 12:03:17 -0400

    Added wakeup and setThickness, changed handshake

--- src/printer/commander/peripage/index.ts
+++ src/printer/commander/peripage/index.ts
@@ -1,12 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as escpos from '../escpos';
 
 const handshake = async (): Promise<Buffer[]> => {
   return [
     // 16, -1, -2, 1, 27, 64, 0
-    Buffer.from(`\x10\xff\xfe\x01\x1b\x40\x00`, 'ascii'),
+    // 16, -1, -2, 1 are the enable bytes idk what 27, 64, 0 is.
+    // extracted bytes: \x1b\x40\x00
+    Buffer.from(`\x10\xff\xfe\x01`, 'ascii'),
   ];
 };
 
-export { handshake };
+const wakeup = async (): Promise<Buffer[]> => {
+  return [
+    // Apparently 12 bytes of 0s wakes up a sleepy printer :\
+    Buffer.from(`\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00`, 'ascii'),
+  ];
+};
+
+const setThickness = async (thickness: number): Promise<Buffer[]> => {
+  return [
+    // 16, -1, 16, 0, thickness
+    Buffer.from(`\x10\xff\x10\x00` + thickness.toString(), 'ascii'),
+  ];
+};
+
+export { handshake, wakeup, setThickness };
 
 export { feed, raster as image } from '../escpos';
