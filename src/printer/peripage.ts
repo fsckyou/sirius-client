6a613d48b0de22ae05d27ace9710794295442dbc
John Scimone <john@scimone.net>
2020-05-24 01:08:29 -0400

    removed more shite

--- src/printer/peripage.ts
+++ src/printer/peripage.ts
@@ -41,7 +41,6 @@ export default class PeripagePrinter implements PrintableImageHandler {
     await this.transport.connect();
 
     await this.write(await peripage.handshake());
-    await this.write(await peripage.wakeup());
     await this.write(await peripage.setPowerOffTime(60 * 24));
   }
 
@@ -55,7 +54,6 @@ export default class PeripagePrinter implements PrintableImageHandler {
     try {
       const bits = await image.asBIN();
       await this.write(await peripage.handshake());
-      await this.write(await peripage.wakeup());
       await this.write(await peripage.image(bits, this.parameters.image.width));
       await this.write(await peripage.feed(3));
     } catch (error) {
